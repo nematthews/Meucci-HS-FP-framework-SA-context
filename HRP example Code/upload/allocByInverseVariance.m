a8fbd2958d3e38243fc510eaaa3971d25ca27d17
Nina Matthews <ninaematthews@gmail.com>
2022-10-07 14:05:56 +0200

    Removing duplicates

--- HRP example Code/upload/allocByInverseVariance.m
+++
@@ -1,8 +0,0 @@
-function pwgt= allocByInverseVariance(assetCovar)
-% allocByInverseVariance performs simple inverse-variance allocation.
-
-% Copyright 2019 The MathWorks, Inc.
-
-pwgt = 1./diag(assetCovar);
-pwgt = pwgt/sum(pwgt);
-end
