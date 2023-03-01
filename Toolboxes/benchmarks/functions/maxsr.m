b75844bcdc04e69dc681514b2b464911935e3369
Nina Matthews <ninaematthews@gmail.com>
2023-03-01 17:26:32 +0200

    Toolbox restructure

---
+++ Toolboxes/benchmarks/functions/maxsr.m
@@ -0,0 +1,13 @@
+function new_weights = Fn_Max_SR(current_weights, pricesTT)
+% Using MV asset allocation
+
+nAssets = size(pricesTT, 2);
+assetReturns = tick2ret(pricesTT);
+% Max 25% into a single asset (including cash)
+p = Portfolio('NumAssets',nAssets,...
+    'LowerBound',0,'UpperBound',0.1,...
+    'LowerBudget',1,'UpperBudget',1);
+p = estimateAssetMoments(p, assetReturns{:,:});
+new_weights = estimateMaxSharpeRatio(p);
+
+end
