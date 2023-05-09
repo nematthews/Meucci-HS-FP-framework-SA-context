223e926926e8ca2eb9adf3b26c67e4bdb54430cd
Nina Matthews <ninaematthews@gmail.com>
2023-05-09 10:10:48 +0200

    Main Change: importfields2keep fn added. Additional change: file structure changes

--- Toolboxes/backtest/SampleFixContents.m
+++ Toolboxes/backtest/SampleFixContents.m
@@ -1,15 +1,92 @@
-% Contents File for  file 
+% dataprocessing Toolbox 
+% Version 1.0 (R2022b) 17-April-2023 
+%
+% Author: 	     Nina Matthews
+% Supervisor: 	 A/Prof. Tim Gebbie
+% Project: 		 Time- and Market-Conditioned Flexible Probabilities: 
+%		  		 in the Context of the South African Bond Market.
+% Last edit: 	 17/04/2023
+% Resources:     The MathWorks, Inc. (2019)  
+%
+%   This program was developed to be utilised in my MSc Dissertation. 
+%   The dataprocessing package is used to prepare raw market data obtained
+%   from financial sources such as Bloomberg. The Toolbox caters for
+%   importing options, dealing with NA values in timetables and adjusting 
+%   misaligned dates within timetables. 
+%   The functions come WITHOUT ANY WARRANTY.
+%
+% NO WARRANTY
+%
+% General information:
 
-% HRP_Fn: 
-% 
-% - Successfully tested using a 10 asset test case data supplied by Kipnis for the De Prodo 2016 paper. 
-% - Output was compared to that in the paper as well as self-coded output generated using R.
-% 
-% MV_Fn:
-% - Fn_Max_SR is a MV optimization using the Sharpe Ratio for the optimization objective. 
-% 
-% 
 % Benchmark_Fns:
 % 
-% - Fn_EW is an equally weighted portfolio solution
-% - Fn_Markowitz_RA is a MV optimisation, with max return, min variance and a fixed risk aversion coefficient factored into the objective function. and a fully invested budget constraint. The optimization makes use of quadratic programming 
+% 
+% -   . The optimization makes use of quadratic programming 
+
+%   This toolbox houses xxxxxxxxxxxxx:
+%
+%	Step 1: 
+%	Step 2: 
+%	Step 3: 
+%
+% General Functions:
+%   Fn_EW - generates an equally weighted portfolio solution
+%   Fn_Markowitz_RA - MV quadratic programming optimisation, with contraints:
+%                           - max return, 
+%                           - min variance,
+%                           - fixed risk aversion coefficient,
+%                           - fully invested budget constraint.
+%   xxxxxxxx - 
+%
+% Test Code
+%   xxxxxxx - 
+%
+% ############# EDIT BELOW ##########################
+% The hrpestimate() function wraps all other sub function files needed for
+% the HRP weights estimation into one implementation, executing all steps:
+%
+% The order of the files listed below correspond to their call order and 
+% their nesting structure, see the individual function files for further 
+% details on inputs and outputs of each.
+% 1. hrpestimate(cov):
+%   1.1 qdiag(link)
+%        1.1.1 leafnodes(rootGroupNodeId, link)
+%   1.2 hrpbisect(cov, sortedIdx)
+%            1.2.1 ivp(cov(sortedIdx, sortedIdx))
+%
+% Test Data
+%   Kipnis_covMat.csv
+% 
+% Obsolete functions.
+%   None at this time.
+%
+% Others
+%   None at this time.
+%
+% GUI Utilities.
+%   None at this time.
+%
+% Copyright(c) 2023 Nina Matthews
+%
+%
+% 1. BECAUSE THE PROGRAM IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY
+% FOR THE PROGRAM, TO THE EXTENT PERMITTED BY APPLICABLE LAW.  EXCEPT WHEN
+% OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES
+% PROVIDE THE PROGRAM "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED
+% OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+% MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  THE ENTIRE RISK AS
+% TO THE QUALITY AND PERFORMANCE OF THE PROGRAM IS WITH YOU.  SHOULD THE
+% PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL NECESSARY SERVICING,
+% REPAIR OR CORRECTION.
+%
+% 2. IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING
+% WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MAY MODIFY AND/OR
+% REDISTRIBUTE THE PROGRAM AS PERMITTED ABOVE, BE LIABLE TO YOU FOR DAMAGES,
+% INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES ARISING
+% OUT OF THE USE OR INABILITY TO USE THE PROGRAM (INCLUDING BUT NOT LIMITED
+% TO LOSS OF DATA OR DATA BEING RENDERED INACCURATE OR LOSSES SUSTAINED BY
+% YOU OR THIRD PARTIES OR A FAILURE OF THE PROGRAM TO OPERATE WITH ANY OTHER
+% PROGRAMS), EVEN IF SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE
+% POSSIBILITY OF SUCH DAMAGES.
+%
---
+++ Toolboxes/dataprocessing/functions/importfields2keep.m
@@ -0,0 +1,30 @@
+function ticker_timetable = importfields2keep(fields,fileName) 
+% Loops through sheets of excel doc, where each sheet stores 
+% individual ticker data to import selected fields from all sheets
+
+% INPUTS:
+% fields = required fields to store eg. 'PX_LAST' or 'CHG_PCT_1M'
+% (type: cell array)
+% fileName = file and its directory path
+% (type: string)
+
+% define columns to keep from each sheet
+fieldsToKeep = fields;
+
+% create sheetName var to loop through in the file
+excelSheetNames = sheetnames(fileName);
+% Preallocate cell array for storage before looping
+ticker_timetable{numel(excelSheetNames)} = [];
+% Load the dataset by sheet
+for sheet = 1:numel(excelSheetNames)
+ticker_timetable{sheet} = readtimetable(fileName,'Sheet',excelSheetNames{sheet}, ...
+    VariableNamingRule='preserve', VariableNamesRange ='A6', ...
+    VariableDescriptionsRange = 'B4',Range='A6');
+% Assign the ticker as the timetable Description
+ticker_timetable{sheet}.Properties.Description = excelSheetNames{sheet};
+% from all columns remove all but 'varsToKeep'
+fieldsToRemove = setdiff(ticker_timetable{sheet}.Properties.VariableNames, fieldsToKeep);
+if ~isempty(fieldsToRemove)
+    ticker_timetable{sheet} = removevars(ticker_timetable{sheet}, fieldsToRemove);
+end
+end
