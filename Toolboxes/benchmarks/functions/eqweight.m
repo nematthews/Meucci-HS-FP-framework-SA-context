9b701c736d010f465139357a904995fc746dbde2
Nina Matthews <ninaematthews@gmail.com>
2023-05-21 19:56:45 +0200

    Main Addition: unadjusted backtest. Overlapping working & plotted

--- Toolboxes/benchmarks/functions/eqweight.m
+++ Toolboxes/benchmarks/functions/eqweight.m
@@ -1,10 +1,18 @@
+function new_weights = eqweight(asset_ob)
+% Equal-weighted portfolio allocation based on number of assets in returns
 
+% If asset_ob is a timetable: Number of assets
+if istimetable(asset_ob)
+    nAssets = width(asset_ob);
+% If asset_ob is an array or matrix: Number of assets
+else
+    nAssets = size(asset_ob, 2);
+end
 
-function new_weights = Fn_EW(current_weights, pricesTT)
-% Equal-weighted portfolio allocation
+% Allocate weight of 1 to each asset
+new_weights = ones(1, nAssets);
 
-nAssets = size(pricesTT, 2);
-new_weights = ones(1,nAssets);
+% Divide by the total number of assets to get equal weights
 new_weights = new_weights / sum(new_weights);
 
 end
