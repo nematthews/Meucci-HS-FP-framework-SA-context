9f622758e9d078cea9de4e21d83034ce83b6b22b
Nina Matthews <ninaematthews@gmail.com>
2023-06-07 11:04:44 +0200

    Inputs included in fns

--- Toolboxes/hrp/functions/ivp.m
+++ Toolboxes/hrp/functions/ivp.m
@@ -1,9 +1,11 @@
 function pw= ivp(AssetCov)
 % ivp performs simple inverse-variance allocation:
 % Inverse Variance Portfolio (IVP)
+%INPUTS:
+% Assectov - covaraince matrix of asset returns
 
-% Copyright 2019 The MathWorks, Inc.
 
+%%
 pw= 1./diag(AssetCov);
 pw = pw/sum(pw);
 end
