16004715ea76f0354dd321a0a703cd7101b34d49
Nina Matthews <ninaematthews@gmail.com>
2023-07-12 21:53:59 +0200

    Adding Sharpe maximizing function into backtest framework. Need to make progress on adding SR values to plots. Slightly  concerned about SR portfolio performance.

--- Toolboxes/benchmarks/functions/maxsr.m
+++ Toolboxes/benchmarks/functions/maxsr.m
@@ -1,16 +1,43 @@
-function new_weights = Fn_Max_SR(current_weights, pricesTT)
+function [SR_opt_wts,MV_SR_sdv, MV_SR_return,frontier_rsk_vec, frontier_return_vec] = maxsr(AssetList,AssetMean,AssetCovar, Rfr)
 % Using MV asset allocation
 
+% Mean-Variance Opimation with a Maximum Sharpe Ratio objective function
+% for portfolio weights allocation.
 
-%%% Still to fix %%%%
-%% 
-nAssets = size(pricesTT, 2);
-assetReturns = tick2ret(pricesTT);
-% Max 25% into a single asset (including cash)
-p = Portfolio('NumAssets',nAssets,...
-    'LowerBound',0,'UpperBound',0.1,...
-    'LowerBudget',1,'UpperBudget',1);
-p = estimateAssetMoments(p, assetReturns{:,:});
-new_weights = estimateMaxSharpeRatio(p);
+% INPUTS:
+% AssetReturns: object consisting of J assets and their returns 
+% over time period T
+% (Type: timetable [T x J])
+
+%% Set up data for MV SR portfolio object
+
+
+%% Set up portfolio object
+p = Portfolio('AssetList', AssetList, 'RiskFreeRate', Rfr);
+p = setAssetMoments(p, AssetMean, AssetCovar);
+% Set initial weights as 1/N portfolio
+% p = setInitPort(p, 1/p.NumAssets);
+% Estimated risk & return using EW port
+% [EW_rsk, EW_ret] = estimatePortMoments(p, p.InitPort);
+
+%% Set Efficient Frontier
+% Set up portfolio constraints for fully-invested long-only portfolios
+% (nonnegative weights that sum to 1)
+p = setDefaultConstraints(p);
+% Construct EFrontier
+pwgt = estimateFrontier(p, 20);
+% Vector of Risk & return across frontier
+[frontier_rsk_vec, frontier_return_vec] = estimatePortMoments(p, pwgt);
+%% Optimize for Sharpe Ratio
+% Estimate portfolio wts with maximum SR objective
+SR_wts = estimateMaxSharpeRatio(p,'Method','iterative');
+SR_opt_wts = SR_wts';
+% Estimate risk and return for optimal port
+[MV_SR_sdv, MV_SR_return] = estimatePortMoments(p,SR_wts);
 
 end
+
+
+
+
+
