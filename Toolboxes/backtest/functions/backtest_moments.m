5e3ee178e978d1f1e8055f0e01769e029018f78a
Nina Matthews <ninaematthews@gmail.com>
2023-08-10 14:47:02 +0200

    Backtests working with HSFP framework. Seeing postive results with ew_ensemble and SA money supply and SAVI40. Presumable as one is leading indicator and other volatility.

--- Toolboxes/backtest/functions/backtest_moments.m
+++ Toolboxes/backtest/functions/backtest_moments.m
@@ -33,12 +33,12 @@ function [hsfp_mu, hsfp_cov,p] = backtest_moments(backtest_object,Window,t)
 % to calculate FProbs given an method that is selected.
 % (Type: 1x1 struct with 9 fields)
 % hsfp_parameters = struct('window',[],'tau',[],'alpha',[],'z_target',[],
-% 'gamma',[],'h',[], 'EPool_prior',[], 'z_target_method', [],'ensemble_wt_method',[]);
+% 'gamma',[],'h',[], 'tau_prior',[],'ensemble_wt_method',[]);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Example: backtest_object expected  (this case window needed for rolling_w)
 % hsfp_parameters = struct('window',[],'tau',[],'alpha',[],'z_target',[],
-% 'gamma',[],'h',[], 'EPool_prior',[], 'z_target_method', [],'ensemble_wt_method',[]);
+% 'gamma',[],'h',[], 'tau_prior',[],'ensemble_wt_method',[]);
 % hsfp_parameters.window = Window_len (NOTE: cannot exceed Window length
 % specified in backtest_analysis.m Fn.)
 % backtest_object = struct('returns',[],'signals',[],'method',[], 'parameters',[]);
@@ -66,15 +66,16 @@ elseif strcmp(backtest_object.method, 'kernel')
 
 elseif strcmp(backtest_object.method, 'e_pooling')
     p = ep_probs(backtest_object.signals, backtest_object.parameters.alpha, ...
-        backtest_object.parameters.z_target,backtest_object.parameters.tau_prior);
+        backtest_object.parameters.tau_prior,backtest_object.parameters.z_target);
 
 elseif strcmp(backtest_object.method, 'ew_ensemble')
     p = ew_ensemble(backtest_object.signals, backtest_object.parameters.alpha, ...
-        backtest_object.parameters.EPool_prior,backtest_object.parameters.z_target_method);
+        backtest_object.parameters.tau_prior,backtest_object.parameters.z_target);
 
 elseif strcmp(backtest_object.method, 'cb_ensemble')
     p = cb_ensemble(backtest_object.signals, backtest_object.parameters.alpha, ...
-        backtest_object.parameters.EPool_prior,backtest_object.parameters.z_target_method);
+        backtest_object.parameters.tau_prior,backtest_object.parameters.z_target, ...
+        backtest_object.parameters.ensemble_wt_method);
 end
 
 [hsfp_mu , hsfp_cov] = hsfp_moments(p,backtest_object.returns);
