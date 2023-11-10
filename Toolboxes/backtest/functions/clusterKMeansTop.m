034a65e2a110ad1293f3bdc0b87824e985f2571d
Nina Matthews <ninaematthews@gmail.com>
2023-11-10 15:14:10 +0200

    Reverted base kmeans fn back to the original dictionary setup

--- Toolboxes/backtest/functions/clusterKMeansTop.m
+++ Toolboxes/backtest/functions/clusterKMeansTop.m
@@ -10,7 +10,7 @@ function [redoClusters,corr1, clstrs, silh] = clusterKMeansTop(corr0, max_K, n_i
 %
 % "For each target number of clusters, we perform a stochastic optimization,
 % repeating the clustering operation n_init times. Among all the clustering
-% alternatives, we choose the solution that achieves the highest quality 
+% alternatives, we choose the solution that achieves the highest quality
 % score, defined as the t-value of the silhouette scores."
 %
 %% INPUTS:
@@ -19,99 +19,108 @@ function [redoClusters,corr1, clstrs, silh] = clusterKMeansTop(corr0, max_K, n_i
 % (type: double, [M x M])
 %
 % max_K - user defined maximum number of clusters to generate by K-mean.
-% (type: double) 
+% (type: double)
 %
 % n_init - number of times the clustering process needs to be repeated.
-% (type: double) 
+% (type: double)
 %
 % options - user defined options that are provided to the kmeans algorithm.
-% Used to implement parallel computing abilities. 
+% Used to implement parallel computing abilities.
 % E.g.:
 % stream = RandStream('mlfg6331_64');  % Random number stream
 % options = statset('UseParallel',1,'UseSubstreams',1,...
 %     'Streams',stream);
 % (type: structure)
-%% 
+%%
 
-    % Check if maxNumClusters and n_init are provided, otherwise use default values
-    if nargin < 2
-        max_K = 10;
+% Check if maxNumClusters and n_init are provided, otherwise use default values
+if nargin < 2
+    max_K = 10;
+end
+%%%% NOTE: max_K can't exceed number of paths
+if max_K > size(corr0,1)
+    max_K = size(corr0,1)-1;
+end
+if nargin < 3
+    n_init = 10;
+end
+
+if nargin < 4
+    options = [];
+end
+
+%% Perform base cluster to initialise E[K]
+[corr1, clstrs, silh] = clusterKMeansBase(corr0, max_K, n_init,options);
+% Calc Tstats per cluster
+clusterTstats = dictionary;
+
+for i = 1:length(keys(clstrs))
+    % Turn each string element in cells to a double
+    series_indx = cellfun(@(x)str2double(x), clstrs{i});
+    % Calculate each cluster's Tstat
+    clusterTstats{i} = mean(silh(series_indx)) / std(silh(series_indx));
+end
+
+% calc Average tstat across clusters
+tStatMean = mean(cell2mat(values(clusterTstats)));
+
+%% extract cluster # that have tstat less than average
+redoClusters = find(cell2mat(values(clusterTstats)) < tStatMean);
+
+%% Recluster clusters with low tstats
+if length(redoClusters) <= 2
+    return;
+else
+    disp("Else entered")
+    % Extract all idx values from redo clusters
+    keysRedo = [];
+    for i = redoClusters
+        keysRedo = [keysRedo, clstrs{i}];
     end
+    % convert char to double
+    keysRedo = cellfun(@(x)str2double(x), keysRedo);
+
+    % Index out the correlations in redo clusters to recluster
+    corrTmp = corr0(keysRedo, keysRedo);
+    meanRedoTstat = mean(cell2mat(clusterTstats(redoClusters)));
+    disp("Before recursive call")
+    %% Recursively call itself
+
     %%%% NOTE: max_K can't exceed number of paths
-    if max_K > size(corr0,1)
-            max_K = size(corr0,1)-1;
-    end
-    if nargin < 3
-        n_init = 10;
+    if max_K > size(corrTmp,1)
+        max_K = size(corrTmp,1)-1;
     end
 
-    if nargin < 4
-        options = [];
-    end
-       
-   %% Perform base cluster to initialise E[K]
-    [corr1, clstrs, silh] = clusterKMeansBase(corr0, max_K, n_init,options);
-    % Calc Tstats per cluster
-    clusterTstats = dictionary;
+    [~, clstrs2, ~] = clusterKMeansTop(corrTmp, max_K, n_init);
+    disp("End recursive call")
+    % When the call eventually stops as if statement is met:
+    % Make new outputs, if necessary
 
-    for i = 1:length(keys(clstrs))
-        % Turn each string element in cells to a double
-        series_indx = cellfun(@(x)str2double(x), clstrs{i});
-        % Calculate each cluster's Tstat
-        clusterTstats{i} = mean(silh(series_indx)) / std(silh(series_indx));
+    % Takes all culsters in cluster that are not in RedoClusters and
+    % combines it with the new clusters2
+    % Iterate over keys of clstrs
+    clstrsToKeep = configureDictionary("double","cell");
+    for i = keys(clstrs)
+        % Check if the key is not in redoClusters
+        if ~ismember(i, redoClusters)
+            % Add key-value pair to clstrsNew
+            clstrsToKeep(i) = clstrs{i};
+        end
     end
-    
-    % calc Average tstat across clusters
-    tStatMean = mean(cell2mat(values(clusterTstats)));
-    
-    %% extract cluster # that have tstat less than average
-    redoClusters = find(cell2mat(values(clusterTstats)) < tStatMean);
-    
-    %% Recluster clusters with low tstats
-    if length(redoClusters) <= 2
+
+    [corrNew, clstrsNew, silhNew] = makeNewOutputs(corr0, clstrsToKeep, clstrs2);
+
+
+    newTstatMean = mean(cellfun(@(i) mean(silhNew(clstrsNew(i)))/std(silhNew(clstrsNew(i)), keys(clstrsNew))));
+
+    if newTstatMean <= tStatMean
+        dis("If statement entered")
         return;
     else
-        disp("Else entered")
-        % Extract all idx values from redo clusters
-        keysRedo = [];
-        for i = redoClusters
-            keysRedo = [keysRedo, clstrs{i}];
-        end
-        % convert char to double
-        keysRedo = cellfun(@(x)str2double(x), keysRedo);
-        
-        % Index out the correlations in redo clusters to recluster
-        corrTmp = corr0(keysRedo, keysRedo);
-        meanRedoTstat = mean(cell2mat(clusterTstats(redoClusters))); 
-         disp("Before recursive call")
-        %% Recursively call itself
-        
-        %%%% NOTE: max_K can't exceed number of paths
-        if max_K > size(corrTmp,1)
-            max_K = size(corrTmp,1)-1;
-        end
-
-        [~, clstrs2, ~] = clusterKMeansTop(corrTmp, max_K, n_init);
-        disp("End recursive call")
-        % When the call eventually stops as if statement is met:
-        % Make new outputs, if necessary
-
-        % Takes all culsters in cluster that are not in RedoClusters and
-        % combines it with the new clusters2
-
-        [corrNew, clstrsNew, silhNew] = makeNewOutputs(corr0, containers.Map(clstrs), clstrs2);
-
-
-        newTstatMean = mean(cellfun(@(i) mean(silhNew(clstrsNew(i)))/std(silhNew(clstrsNew(i)), keys(clstrsNew))));
-
-        if newTstatMean <= tStatMean
-            dis("If statement entered")
-            return;
-        else
-            corr1 = corrNew;
-            clstrs = clstrsNew;
-            silh = silhNew;
-              dis("Else ENDED")
-        end
+        corr1 = corrNew;
+        clstrs = clstrsNew;
+        silh = silhNew;
+        dis("Else ENDED")
     end
+end
 end
